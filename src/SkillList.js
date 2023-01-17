4aba5129f0e73f1e90e07c1ea88b32247b640d9c
marvinraydalida <marvinraydalida@gmail.com>
2023-01-17 22:31:42 +0800

    Skills grid fix
    Fixed grid transition issues. Updated grid images as well.

---
+++ src/SkillList.js
@@ -0,0 +1,65 @@
+export const languages = [
+  {
+    name: "HTML 5",
+    description:
+      "HTML is the language that introduced me to coding. It's one of the core technologies used for creating and publishing content on the World Wide Web, and is often the first language that people learn when they're starting to explore the world of coding.",
+  },
+  {
+    name: "CSS 3",
+    description:
+      "You can't mention HTML without mentioning CSS, because they're like bread and butter. As a frontend developer, I find that CSS takes up a lot of my time because it involves both coding and design. It's an essential part of creating engaging and visually appealing web pages.",
+  },
+  {
+    name: "PHP",
+    description:
+      "PHP was my introduction to backend development, and it taught me a lot about working with databases and APIs. With PHP, I've built some web applications from start to finish, including both the frontend and backend components.",
+  },
+  {
+    name: "Python",
+    description:
+      "Python is an excellent programming language for beginners because it's simple, yet powerful. With Python, I have been able to automate tedious and repetitive tasks, such as data cleansing, which has saved me a lot of time and effort. I highly recommend Python to anyone looking to get started in programming.",
+  },
+  {
+    name: "JavaScript",
+    description:
+      "JavaScript is crucial for adding interactivity to the frontend of my web applications, which helps keep users engaged. I love working with JavaScript because it allows me to manipulate elements and objects on the page using the Document Object Model (DOM). It's a powerful and fun language to work with.",
+  },
+  {
+    name: "C",
+    description:
+      "C was my first ever programming language, and it introduced me to some of the core concepts of programming such as conditional statements, loops, logic, and data structures. Learning C was a great foundation for my development as a programmer.",
+  },
+];
+
+export const technologies = [
+  {
+    name: "React JS",
+    description:
+      "As I delve deeper into learning JavaScript there's no doubt that along the road I have to learn React JS as well. In fact this portfolio is developed using React JS to create a single page application.",
+  },
+  {
+    name: "CodeIgniter 3",
+    description:
+      "CodeIgniter 3 introduced me to web frameworks as well as PHP. It gave me an idea with the importance of code reusability and boilerplates, which provide easier development process. CodeIgniter 3 also offers some security features for CSRF, form validation, and Cross Site Scripting (XSS). Fun fact with CodeIgniter 3, I started learning through documentations as well.",
+  },
+  {
+    name: "Django",
+    description:
+      "In essence Django is just another web framework, like CodeIgniter 3, but requires a different programming language to develop. But for machine learning application Django is a perfect fit, as you can load a machine learning model through it and create web application over it.",
+  },
+  {
+    name: "FastAPI",
+    description:
+      "For stand alone RESTful API my choice is FastAPI, as it is a minimalistic and a lightweight framework. It also offers interactive documentation for API endpoints, which helps the developers understand the necessary parameters, request methods, and schemas required when using my API. FastAPI is also our choice for our thesis project, which is a Nudity  classification and detection API.",
+  },
+  {
+    name: "Bootstrap",
+    description:
+      "With Bootstrap I can easily create simple user interface for some of my projects. Bootstrap offers responsive features as well as ready to use forms and components.",
+  },
+  {
+    name: "Express JS",
+    description:
+      "To be honest I have never applied Express JS to some of my school projects but I started learning it to further understand authentication using JSON web tokens as well as learn MongoDB.",
+  },
+];
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,52 +1,30 @@
 import { useState, useEffect, useRef } from "react";
 
-import SkillTitle from "./SkillTitle";
-import SkillGrid from "./SkillGrid";
-import SkillDescription from "./SkillDescription";
+import { languages, technologies } from "../SkillList";
 
-const languages = [
-  {
-    name: "HTML 5",
-    description:
-      "HTML is the language that introduced me to coding. It's one of the core technologies used for creating and publishing content on the World Wide Web, and is often the first language that people learn when they're starting to explore the world of coding.",
-  },
-  {
-    name: "CSS 3",
-    description:
-      "You can't mention HTML without mentioning CSS, because they're like bread and butter. As a frontend developer, I find that CSS takes up a lot of my time because it involves both coding and design. It's an essential part of creating engaging and visually appealing web pages.",
-  },
-  {
-    name: "PHP",
-    description:
-      "PHP was my introduction to backend development, and it taught me a lot about working with databases and APIs. With PHP, I've built some web applications from start to finish, including both the frontend and backend components.",
-  },
-  {
-    name: "Python",
-    description:
-      "Python is an excellent programming language for beginners because it's simple, yet powerful. With Python, I have been able to automate tedious and repetitive tasks, such as data cleansing, which has saved me a lot of time and effort. I highly recommend Python to anyone looking to get started in programming.",
-  },
-  {
-    name: "JavaScript",
-    description:
-      "JavaScript is crucial for adding interactivity to the frontend of my web applications, which helps keep users engaged. I love working with JavaScript because it allows me to manipulate elements and objects on the page using the Document Object Model (DOM). It's a powerful and fun language to work with.",
-  },
-  {
-    name: "C",
-    description:
-      "C was my first ever programming language, and it introduced me to some of the core concepts of programming such as conditional statements, loops, logic, and data structures. Learning C was a great foundation for my development as a programmer.",
-  },
-];
+import SkillDescriptionContainer from "./SkillDescriptionContainer";
+import SkillGrid from "./SkillGrid";
+
+// import SkillTitle from "./SkillTitle";
+// import SkillDescription from "./SkillDescription";
 
 function Skill() {
   const languagesSectionRef = useRef(null);
+  const technologySectionRef = useRef(null);
 
-  const previousActiveGrid = useRef(null);
-  const [active, setActive] = useState(false);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const languagePreviousActiveGrid = useRef(null);
+  const technologyPreviousActiveGrid = useRef(null);
+  const [isLanguageActive, setIsLanguageActive] = useState(false);
+  const [isTechnologyActive, setIsTechnologyActive] = useState(false);
+  const [languageName, setLanguageName] = useState("");
+  const [languageDescription, setlanguageDescription] = useState("");
+  const [technologyName, setTechnologyName] = useState("");
+  const [technologyDescription, settTechnologyDescription] = useState("");
 
   const languageNameRef = useRef(null);
   const languageDescriptionRef = useRef(null);
+  const technologyNameRef = useRef(null);
+  const technologyDescriptionRef = useRef(null);
 
   function onSelectLanguage(event) {
     const selectedLanguage = languages.find(
@@ -55,85 +33,134 @@ function Skill() {
 
     event.target.classList.toggle("selected");
 
-    setTitle(selectedLanguage.name);
-    setDescription(selectedLanguage.description);
+    setLanguageName(selectedLanguage.name);
+    setlanguageDescription(selectedLanguage.description);
 
-    if (previousActiveGrid.current !== null) {
-      if (previousActiveGrid.current !== event.target) {
-        previousActiveGrid.current.classList.toggle("selected");
-        previousActiveGrid.current = event.target;
+    if (languagePreviousActiveGrid.current !== null) {
+      if (languagePreviousActiveGrid.current !== event.target) {
+        languagePreviousActiveGrid.current.classList.toggle("selected");
+        languagePreviousActiveGrid.current = event.target;
       } else {
-        previousActiveGrid.current = null;
-        setTitle("");
-        setActive(false);
+        languagePreviousActiveGrid.current = null;
+        setLanguageName("");
+        setIsLanguageActive(false);
       }
     } else {
-      previousActiveGrid.current = event.target;
-      setActive(true);
+      languagePreviousActiveGrid.current = event.target;
+      setIsLanguageActive(true);
+    }
+  }
+
+  function onSelectTechnology(event) {
+    const selectedLanguage = technologies.find(
+      (language) => language.name === event.target.dataset.name
+    );
+
+    event.target.classList.toggle("selected");
+
+    setTechnologyName(selectedLanguage.name);
+    settTechnologyDescription(selectedLanguage.description);
+
+    if (technologyPreviousActiveGrid.current !== null) {
+      if (technologyPreviousActiveGrid.current !== event.target) {
+        technologyPreviousActiveGrid.current.classList.toggle("selected");
+        technologyPreviousActiveGrid.current = event.target;
+      } else {
+        technologyPreviousActiveGrid.current = null;
+        setTechnologyName("");
+        setIsTechnologyActive(false);
+      }
+    } else {
+      technologyPreviousActiveGrid.current = event.target;
+      setIsTechnologyActive(true);
     }
   }
 
   useEffect(() => {
     document.body.className = "scroll-body";
+
+    const timeOut = setTimeout;
+
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
+
+          timeOut(() => {
+            entry.target.style.transitionDelay = "0ms"
+          }, 250)
         }
       });
     });
 
-    for (const container of languagesSectionRef.current.children) {
+    const languageContainer = languagesSectionRef.current.children;
+    const languageGrid = languagesSectionRef.current.lastElementChild.children;
+
+    const technologyContainer = technologySectionRef.current.children;
+    const technologyGrid =
+      technologySectionRef.current.firstElementChild.children;
+
+    for (const container of languageContainer) {
       observer.observe(container);
     }
-    for (const grid of languagesSectionRef.current.lastElementChild.children) {
+
+    for (const grid of languageGrid) {
       observer.observe(grid);
     }
-    const timeOut = setTimeout(() => {
-      for (const grid of languagesSectionRef.current.lastElementChild
-        .children) {
-        grid.classList.remove("hidden");
-        grid.classList.remove("show");
-      }
-    }, 1500);
 
-    //Clear interval to avoid memory leak
+    for (const container of technologyContainer) {
+      observer.observe(container);
+    }
+
+    for (const grid of technologyGrid) {
+      observer.observe(grid);
+    }
+
     return () => clearTimeout(timeOut);
   }, []);
 
   useEffect(() => {
-    let timeOut;
     if (languageNameRef.current !== null) {
       languageNameRef.current.classList.add("slide-in");
       languageDescriptionRef.current.classList.add("slide-in");
-      timeOut = setTimeout(() => {
-        languageNameRef.current.classList.remove("slide-in");
-        languageDescriptionRef.current.classList.remove("slide-in");
-      }, 250);
     }
 
+    const timeOut = setTimeout(() => {
+      if (languageNameRef.current !== null) {
+        languageNameRef.current.classList.remove("slide-in");
+        languageDescriptionRef.current.classList.remove("slide-in");
+      }
+    }, 250);
+
     return () => clearTimeout(timeOut);
-  }, [title]);
+  }, [languageName]);
+
+  useEffect(() => {
+    if (technologyNameRef.current !== null) {
+      technologyNameRef.current.classList.add("slide-in");
+      technologyDescriptionRef.current.classList.add("slide-in");
+    }
+
+    const timeOut = setTimeout(() => {
+      if (technologyNameRef.current !== null) {
+        technologyNameRef.current.classList.remove("slide-in");
+        technologyDescriptionRef.current.classList.remove("slide-in");
+      }
+    }, 250);
+
+    return () => clearTimeout(timeOut);
+  }, [technologyName]);
 
   return (
     <>
       <section className="skill-section" ref={languagesSectionRef}>
-        <div className="container hidden">
-          {active ? (
-            <SkillDescription
-              title={title}
-              description={description}
-              titleRef={languageNameRef}
-              descriptionRef={languageDescriptionRef}
-            />
-          ) : (
-            <SkillTitle
-              firstText={"Programming &"}
-              secondText={"Markup"}
-              thirdText={"Languages"}
-            />
-          )}
-        </div>
+        <SkillDescriptionContainer
+          title={languageName}
+          description={languageDescription}
+          titleRef={languageNameRef}
+          descriptionRef={languageDescriptionRef}
+          isActive={isLanguageActive}
+        />
         <div className="container skill-grid-container hidden">
           <SkillGrid
             source="/images/html.svg"
@@ -143,6 +170,11 @@ function Skill() {
           <SkillGrid
             source="/images/css.svg"
             alt="CSS 3"
+            onClick={onSelectLanguage}
+          />
+          <SkillGrid
+            source="/images/javascript.svg"
+            alt="JavaScript"
             onClick={onSelectLanguage}
           />
           <SkillGrid
@@ -156,16 +188,67 @@ function Skill() {
             onClick={onSelectLanguage}
           />
           <SkillGrid
-            source="/images/javascript.svg"
-            alt="JavaScript"
-            onClick={onSelectLanguage}
-          />
-          <SkillGrid
             source="/images/c.svg"
             alt="C"
             onClick={onSelectLanguage}
           />
         </div>
+      </section>
+      <section className="skill-section" ref={technologySectionRef}>
+        <div className="container skill-grid-container hidden">
+          <SkillGrid
+            source="/images/react.svg"
+            alt="React JS"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/codeigniter.svg"
+            alt="CodeIgniter 3"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/django.svg"
+            alt="Django"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/fastapi.svg"
+            alt="FastAPI"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/bootstrap.svg"
+            alt="Bootstrap"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/express.svg"
+            alt="Express JS"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/tensorflow.svg"
+            alt="TensorFlow"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/mysql.svg"
+            alt="MySQL"
+            onClick={onSelectTechnology}
+          />
+          <SkillGrid
+            source="/images/mongodb.svg"
+            alt="Mongo DB"
+            onClick={onSelectTechnology}
+          />
+        </div>
+        <SkillDescriptionContainer
+          title={technologyName}
+          description={technologyDescription}
+          titleRef={technologyNameRef}
+          descriptionRef={technologyDescriptionRef}
+          isActive={isTechnologyActive}
+        />
       </section>
     </>
   );
