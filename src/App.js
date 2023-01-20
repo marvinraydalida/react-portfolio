853a26ca9f1fcf370f1ca2c8ab7ed5e1db1bffb4
marvinraydalida <marvinraydalida@gmail.com>
2023-01-21 01:24:48 +0800

    Added projects page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Project from "./components/Project";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home className="home" />} />
         <Route path="/about" element={<About className="about"/>} />
         <Route path="/skill" element={<Skill className="skill"/>} />
+        <Route path="/project" element={<Project className="project"/>} />
       </Routes>
     </>
   );
---
+++ src/components/Project.js
@@ -0,0 +1,97 @@
+import ProjectGrid from "./ProjectGrid";
+
+function Project() {
+  return (
+    <section className="project-section">
+      <h1>
+        <span>self.</span>works
+      </h1>
+      <p>
+        Here are some of the projects that I have developed and have been
+        involved in developing.
+      </p>
+
+      <div className="project-container">
+        <ProjectGrid
+          title="Axie Infinity management tracker"
+          description="This tracker uses the Axie Infinity v1 API endpoint to enable managers to track the progress of their scholars."
+          technology={[
+            "php",
+            "codeigniter",
+            "javascript",
+            "mysql",
+            "html",
+            "css",
+          ]}
+          code="https://github.com/marvinraydalida/axie-management-tracker"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/E516M7RJG8E"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </ProjectGrid>
+        <ProjectGrid
+          title="Filipino speech to Baybayin script"
+          description="This simple application utilizes the Web Speech API and regular expressions (RegEx) to transcribe Filipino speech into the Baybayin writing system."
+          technology={["javascript", "html", "css"]}
+          code="https://github.com/marvinraydalida/baybayin-transcription"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/1Ye4bzg2kqQ"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </ProjectGrid>
+        <ProjectGrid
+          title="Spot: Music mood prediction"
+          description="Spot music mood prediction is a python based web application that predicts the mood of a track from Spotify."
+          technology={[
+            "python",
+            "django",
+            "sklearn",
+            "javascript",
+            "html",
+            "css",
+          ]}
+          code="https://github.com/marvinraydalida/spot-music-mood-prediction"
+        >
+          <img src="/images/spot.png" alt="" />
+        </ProjectGrid>
+        <ProjectGrid
+          title="Appointment management system"
+          description="This is a dental clinic appointment management system, a system requirement for our Software Engineering 2 subject."
+          technology={[
+            "php",
+            "codeigniter",
+            "javascript",
+            "mysql",
+            "html",
+            "css",
+          ]}
+          code="https://github.com/marvinraydalida/Appointment-system"
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/-c6--puzjjg?start=330"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </ProjectGrid>
+      </div>
+    </section>
+  );
+}
+
+export default Project;
